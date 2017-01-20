40911cb1fc1edc631c30c22b46ab2de29570c07c
Leon Do <leondo123@gmail.com>
2017-01-19 20:05:09 -0500

    asdfas

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser')
 var path = require('path');
 
 //-------------------------------------------------//
 //--------------------Express----------------------//
 //-------------------------------------------------//
 var app = express()
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'home.html'));
@@ -17,6 +20,11 @@ app.get('/reserve', function (req, res) {
 
 app.get('/tables', function (req, res) {
     res.sendFile(path.join(__dirname, 'tables.html'));
+})
+
+
+app.post('/', urlencodedParser, function (req, res){
+    console.log(req.body)
 })
 
 app.listen(3000)
