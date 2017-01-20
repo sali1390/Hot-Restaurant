ebe4951b4636a4dead2a468b58b3589f993f00f0
Saleema Ali <sali1390@gmail.com>
2017-01-19 19:58:26 -0500

    Added arrays

--- server.js
+++ server.js
@@ -7,15 +7,18 @@ var path = require('path');
 //-------------------------------------------------//
 var app = express()
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, './home.html'));
-})
+var reservations = [];
+var waitlist = [];
 
 app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'home.html'));
+})
+
+app.get('/reserve', function (req, res) {
     res.sendFile(path.join(__dirname, './reserve.html'));
 })
 
-app.get('/', function (req, res) {
+app.get('/tables', function (req, res) {
     res.sendFile(path.join(__dirname, './tables.html'));
 })
 
